9e5af2a682f2906512650d94f95e5ce255fbce37
Bimendra Bandara <bimendra.85@gmail.com>
2024-04-16 21:34:37 +0800

    Link database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,21 @@ import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/d63e70b2-3c03-42e0-89f8-9a41c47daf5f-hwt9mm.jpg",
-  "https://utfs.io/f/3dc6a465-e53c-4b19-96ad-9098fafda9fd-39brdz.jpg",
-  "https://utfs.io/f/68c3175d-0004-4ff7-afa3-7e7014678c2e-rgqlw6.jpg",
-  "https://utfs.io/f/0285c015-2a34-4e62-9cf6-af1f629d512c-fv3bom.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {mockImages.map(({ id, url }) => (
+        {images.map(({ id, url, name }) => (
           <div key={id} className="w-48 p-4">
             <img src={url} alt="Image" />
+            <span>{name}</span>
           </div>
         ))}
       </div>
-      Hello (gallery in progress)
     </main>
   );
 }
