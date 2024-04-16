6148da7402a150af48612975e4452e77bba2cdfd
Bimendra Bandara <bimendra.85@gmail.com>
2024-04-16 23:09:35 +0800

    Restrict gallery to signed in users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
@@ -8,14 +9,19 @@ export default async function HomePage() {
 
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map(({ id, url, name }) => (
-          <div key={id} className="w-48 p-4">
-            <img src={url} alt="Image" />
-            <span>{name}</span>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl">Please sign in to continue</div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex flex-wrap gap-4">
+          {images.map(({ id, url, name }) => (
+            <div key={id} className="w-48 p-4">
+              <img src={url} alt="Image" />
+              <span>{name}</span>
+            </div>
+          ))}
+        </div>
+      </SignedIn>
     </main>
   );
 }
